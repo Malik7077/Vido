462e5ba2d4dde8f800d98c18e65dcabaf5344417
faiz <Extrafaiz@icloud.com>
2020-08-12 21:44:14 +0200

    added code

---
+++ Vido/Response.swift
@@ -0,0 +1,28 @@
+//
+//  Response.swift
+//  Vido
+//
+//  Created by Bruker on 12.08.2020.
+//  Copyright © 2020 Malik. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items: [Video]?
+    
+    enum CodingKeys:String, CodingKey {
+        
+        case items
+        
+    }
+    
+    init (from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+    }
+    
+}

96b3b8dc2e1f78cf7e1464492ded9fa4cc5931a0
faiz <Extrafaiz@icloud.com>
2020-08-12 18:33:40 +0200

    new lesson

---
+++ Vido/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Vido
+//
+//  Created by Bruker on 11.08.2020.
+//  Copyright © 2020 Malik. All rights reserved.
+//
+
+import Foundation
---
+++ Vido/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Vido
+//
+//  Created by Bruker on 11.08.2020.
+//  Copyright © 2020 Malik. All rights reserved.
+//
+
+import Foundation

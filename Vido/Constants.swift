fe4b5f4fb3127bd213c82d082487300a40cda7a8
faiz <Extrafaiz@icloud.com>
2020-08-15 15:50:23 +0200

    mm

--- Vido/Constants.swift
+++ Vido/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Constants {
 
-    static var API_KEY = "AIzaSyBMpud9tb8NDHJdgO2o9BayoUwogejqYbQ"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
     static var API_URL =   "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY) "
     
